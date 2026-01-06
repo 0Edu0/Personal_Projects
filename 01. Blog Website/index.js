1001b0cb5f944ed5dcae2aacbc8dd30ea9b20989
Eduardo <edulink.1610@gmail.com>
2026-01-05 22:36:24 -0600

    Save text files

--- 01. Blog Website/index.js
+++ 01. Blog Website/index.js
@@ -1,8 +1,11 @@
 import express from "express";
-import body from "body-parser";
+import bodyParser from "body-parser";
+import fs from "fs";
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
@@ -12,6 +15,16 @@ app.get("/", (req, res) => {
 
 app.get("/new-post", (req, res) => {
     res.render('new-post.ejs');
+});
+
+app.post("/submit", (req, res) => {
+    const blogName = req.body.blogName;
+    const blogContent = req.body.content;
+    const blogStoragePath = '/media/eduardo/Seagate/Programming/Personal Projects/Personal_Projects/01. Blog Website/blogsStorage';
+    fs.writeFile(`${blogStoragePath}/${blogName}.txt`, blogContent, (err) => {
+        if(err) throw err;
+        console.log('The file has been saved!');
+    });
 });
 
 app.listen(port, () => {
