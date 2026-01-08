c018755a996cc5adaec2518a6a25e7bb761e513c
Eduardo <edulink.1610@gmail.com>
2026-01-07 21:38:48 -0600

    Show list of text files

--- 01. Blog Website/index.js
+++ 01. Blog Website/index.js
@@ -1,30 +1,44 @@
 import express from "express";
 import bodyParser from "body-parser";
 import fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const port = 3000;
+const blogStoragePath = '/media/eduardo/Seagate/Programming/Personal Projects/Personal_Projects/01. Blog Website/blogsStorage';
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render('index.ejs');
-});
+    fs.readdir(blogStoragePath, (err, files) => {
+        if(err) {
+            console.error('Error reading directory:', err);
+            return res.status(500).send("Server error");
+        }
+        const blogs = files.map(file => 
+            file.replace(/\.txt$/, "")
+        );
+
+        res.render('index.ejs', { blogs });
+        });
+    });
 
 app.get("/new-post", (req, res) => {
     res.render('new-post.ejs');
 });
 
-app.post("/submit", (req, res) => {
+app.post('/submit', (req, res) => {
     const blogName = req.body.blogName;
     const blogContent = req.body.content;
-    const blogStoragePath = '/media/eduardo/Seagate/Programming/Personal Projects/Personal_Projects/01. Blog Website/blogsStorage';
     fs.writeFile(`${blogStoragePath}/${blogName}.txt`, blogContent, (err) => {
         if(err) throw err;
         console.log('The file has been saved!');
     });
+    res.redirect('/');
 });
 
 app.listen(port, () => {
